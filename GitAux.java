0e35ad2ee48b8b2e8737d9db1dd04a81c98e0aaf
Luis Daniel Capel de la Torre <luisdaniel@uma.es>
2013-03-13 18:18:10 +0100

    modificaciones alumno 1

--- GitAux.java
+++ GitAux.java
@@ -11,6 +11,7 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+		System.out.println("modificado por alum 1"); // insertado por alum1
     }
     
     public void metodoAlumno2(){
@@ -19,6 +20,9 @@ public class GitAux {
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+		// modificacaciones alumno 1 
+ 
+		System.out.println("Alumno 1 inserta esta sentencia"); 
     }
 	
     public void testGitAux(){ 
